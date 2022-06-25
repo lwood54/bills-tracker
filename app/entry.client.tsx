9c49c94831824a837deda5b7bd943172f435892e
lwood54 <lwood3499@gmail.com>
2022-06-25 18:03:31 -0500

    update to hydrateRoot to work with React 18

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import { hydrate } from "react-dom";
+// @ts-ignore
+import { hydrateRoot } from "react-dom/client";
 import { CacheProvider } from "@emotion/react";
 import { RemixBrowser } from "@remix-run/react";
 
@@ -24,9 +25,9 @@ function ClientCacheProvider({ children }: ClientCacheProviderProps) {
   );
 }
 
-hydrate(
+hydrateRoot(
+  document,
   <ClientCacheProvider>
     <RemixBrowser />
-  </ClientCacheProvider>,
-  document
+  </ClientCacheProvider>
 );
