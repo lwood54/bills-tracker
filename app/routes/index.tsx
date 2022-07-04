54ce070db3a47d411c803ab87ec78bcafbf0ae34
lwood54 <lwood3499@gmail.com>
2022-07-04 11:02:52 -0500

    rm main wrapper

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -8,38 +8,36 @@ import { BackgroundContainer } from "~/components/BackgroundContainer";
 export default function Index() {
   const user = useOptionalUser();
   return (
-    <main>
-      <BackgroundContainer>
-        {user ? (
-          <Link to="/bills">View Bills for {user.email}</Link>
-        ) : (
-          <Stack
-            alignItems="center"
-            justifyContent="flex-start"
-            p="4"
-            gap="100px"
-          >
-            <Stack direction="column">
-              <Heading as="h3" size="lg" textAlign="center">
-                Welcome to
-              </Heading>
-              <Heading as="h2" size="3xl" noOfLines={1} textAlign="center">
-                The Bills IO
-              </Heading>
-            </Stack>
-            <Stack w="100%" gap="32px" maxW={800}>
-              <Link to={urlPath.LOGIN}>
-                <ButtonBase size="full">Log In</ButtonBase>
-              </Link>
-              <Link to={urlPath.SIGNUP}>
-                <ButtonBase size="full" variant={BTN.SECONDARY}>
-                  Sign Up
-                </ButtonBase>
-              </Link>
-            </Stack>
+    <BackgroundContainer>
+      {user ? (
+        <Link to="/bills">View Bills for {user.email}</Link>
+      ) : (
+        <Stack
+          alignItems="center"
+          justifyContent="flex-start"
+          p="4"
+          gap="100px"
+        >
+          <Stack direction="column">
+            <Heading as="h3" size="lg" textAlign="center">
+              Welcome to
+            </Heading>
+            <Heading as="h2" size="3xl" noOfLines={1} textAlign="center">
+              The Bills IO
+            </Heading>
           </Stack>
-        )}
-      </BackgroundContainer>
-    </main>
+          <Stack w="100%" gap="32px" maxW={800}>
+            <Link to={urlPath.LOGIN}>
+              <ButtonBase size="full">Log In</ButtonBase>
+            </Link>
+            <Link to={urlPath.SIGNUP}>
+              <ButtonBase size="full" variant={BTN.SECONDARY}>
+                Sign Up
+              </ButtonBase>
+            </Link>
+          </Stack>
+        </Stack>
+      )}
+    </BackgroundContainer>
   );
 }
